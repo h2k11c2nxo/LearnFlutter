adc521b62362919007b728aede9c7a4fd6780aca
tuan.lehong <totchuban@gmail.com>
2022-02-11 17:31:58 +0700

    add new file

---
+++ lib/models/fake_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'category.dart';
+ const FAKE_CATEGORIES = const [
+ Category(id: 1, content: 'Japanese\'s Foods', color: Colors.blue),
+];
